86a8f48e2894b6b19e4cb996875969bc0c21b90c
ab-rai <ab.rai32768@gmail.com>
2023-07-07 17:57:31 +0530

    Adding react hooks and shimmer ui

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,22 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+    const [restList,setRestList] = useState([]);
+    useEffect(()=> {
+        async function fetchData(){
+            const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6105073&lng=77.1145653&page_type=DESKTOP_WEB_LISTING');
+            const jsonData = await data.json();
+            setRestList(jsonData?.data?.cards[2]?.data?.data?.cards);
+        }
+        fetchData();
+    }
+        ,[]);
+
+
+    if(restList.length === 0){
+        return <Shimmer/>
+    }
+        
     return (
         <div className="body">
             <div className="search">
                 <h2>Search</h2>
             </div>
             <div className="restaurant-container">
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-                
+                {
+                    restList.map(res => <RestaurantCard 
+                        cloudinaryImageId={res.data.cloudinaryImageId}
+                        name = {res.data.name}
+                        cuisines = {res.data.cuisines}  
+                        rating = {res.data.avgRating}
+                        deliveryTime = {res.data.deliveryTime}  
+                        />)
+                }
             </div>
         </div>
     );
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,11 +1,14 @@
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const {cloudinaryImageId, name, cuisines, avgRating, deliveryTime} = props;
+    const restLogo = `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`;
+
     return (
         <div className="restaurant-card">
-            <img className="restaurant-logo"  src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bsbkwruizgwqmv7cvoaf" alt="rest logo"/>
-            <h3>Restaurants Brand Asia</h3>
-            <h4>Biryani, North Indian, Chinese</h4>
-            <h4>4.4 Stars</h4>
-            <h4>35 minutes</h4>
+            <img className="restaurant-logo"  src={restLogo} alt="rest logo"/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(',  ')}</h4>
+            <h4>{avgRating}</h4>
+            <h4>{deliveryTime}</h4>
         </div>
     );
 }
